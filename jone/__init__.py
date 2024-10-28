8047574fac2f6edbe4b1754a7dbf36ac0e9357fe
Vladimir Kozhukalov <kozhukalov@gmail.com>
2024-10-28 13:17:27 -0500

    Filter nodes by user labels

--- jone/__init__.py
+++ jone/__init__.py
@@ -5,8 +5,12 @@ from pathlib import Path
 
 
 @click.group()
-def jone():
-    pass
+@click.option("--label", "-l", multiple=True,
+              help="Filter VMs by user labels")
+@click.pass_context
+def jone(ctx, label):
+    ctx.ensure_object(dict)
+    ctx.obj['labels'] = label
 
 
 def session():
@@ -19,25 +23,35 @@ def session():
     return pyone.OneServer(endpoint, session=f"{username}:{password}", https_verify=True)
 
 
-def get_vms():
+def get_vms(labels=None):
     one = session()
     vm_pool = one.vmpool.info(-2, -1, -1, -1)
-    return sorted(vm_pool.VM, key=lambda x: x.NAME)
+    vms = vm_pool.VM
+    if labels:
+        vms = []
+        for vm in vm_pool.VM:
+            vm_labels = vm.USER_TEMPLATE.get("LABELS", "").split()
+            for l in labels:
+                if l in vm_labels:
+                    vms.append(vm)
+    return sorted(vms, key=lambda x: x.NAME)
 
 
-def get_vm_ips():
-    return [vm.TEMPLATE["NIC"]["IP"] for vm in get_vms()]
+def get_vm_ips(labels=None):
+    return [vm.TEMPLATE["NIC"]["IP"] for vm in get_vms(labels)]
 
 
 @jone.command()
-def pprint():
-    for vm in get_vms():
+@click.pass_context
+def pprint(ctx):
+    for vm in get_vms(ctx.obj['labels']):
         print(f"VM ID: {vm.ID}")
         print(f"VM Name: {vm.NAME}")
         print(f"VM State: {vm.STATE}")
         print(f"VM LCM State: {vm.LCM_STATE}")
         print(f"VM User: {vm.UNAME}")
         print(f"VM Group: {vm.GNAME}")
+        print(f"VM Labels: {vm.USER_TEMPLATE.get("LABELS")}")
         # print(f"VM TEMPLATE: {vm.TEMPLATE}")
         nics = vm.TEMPLATE.get("NIC")
         if isinstance(nics, list):
@@ -49,29 +63,34 @@ def pprint():
 
 
 @jone.command()
-def ansible_hosts():
+@click.pass_context
+def ansible_hosts(ctx):
     print("  hosts:")
-    for vm in zip(("primary", "node-1", "node-2", "node-3"), get_vms()):
+    for vm in zip(("primary", "node-1", "node-2", "node-3"), get_vms(ctx.obj['labels'])):
         print(f"    {vm[0]}:")
         print(f"      ansible_host: {vm[1].TEMPLATE["NIC"]["IP"]}")
 
 
 @jone.command()
-def all_ips():
-    print(" ".join(get_vm_ips()))
+@click.pass_context
+def all_ips(ctx):
+    print(" ".join(get_vm_ips(ctx.obj['labels'])))
 
 
 @jone.command()
-def cluster_ips():
-    print(" ".join(get_vm_ips()[1:]))
+@click.pass_context
+def cluster_ips(ctx):
+    print(" ".join(get_vm_ips(ctx.obj['labels'])[1:]))
 
 
 @jone.command()
-def primary_ip():
-    print(get_vm_ips()[0])
+@click.pass_context
+def primary_ip(ctx):
+    print(get_vm_ips(ctx.obj['labels'])[0])
 
 
 @jone.command()
+@click.pass_context
 @click.argument('node', nargs=1, type=int)
-def node_ip(node):
-    print(get_vm_ips()[node])
+def node_ip(ctx, node):
+    print(get_vm_ips(ctx.obj['labels'])[node])
